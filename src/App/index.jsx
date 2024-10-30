02d05b87750b16a69a72c387123bbfa335706d0f
David Portilla <david.portilla@mews.com>
2024-10-30 17:23:38 +0100

    adding initial routes

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter, useRoutes } from "react-router-dom";
 import Home from "../Pages/Home";
 import MyAccount from "../Pages/MyAccount";
 import MyOrder from "../Pages/MyOrder";
@@ -5,17 +6,39 @@ import MyOrders from "../Pages/MyOrders";
 import NotFound from "../Pages/NotFound";
 import "./App.css";
 
-function App() {
+const AppRoutes = () => {
+  let routes = useRoutes([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/my-account",
+      element: <MyAccount />,
+    },
+    {
+      path: "/my-order",
+      element: <MyOrder />,
+    },
+    {
+      path: "/my-orders",
+      element: <MyOrders />,
+    },
+    {
+      path: "/*",
+      element: <NotFound />,
+    },
+  ]);
+
+  return routes;
+};
+
+const App = () => {
   return (
-    <>
-      <div className="bg-red-100">Hola mundo</div>
-      <Home />
-      <MyAccount />
-      <MyOrder />
-      <MyOrders />
-      <NotFound />
-    </>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
